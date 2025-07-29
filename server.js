d9838200b158188fc974ca52866bb5749768f0a2
Walmart Fan <163700193+WalmartFan0@users.noreply.github.com>
2025-07-28 22:52:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -104,3 +104,9 @@ app.post('/post', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
